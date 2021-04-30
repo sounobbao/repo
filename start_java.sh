e1994dbd33e116c22a3199df780a7e2ea673f9b3
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-04-30 17:16:39 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -12,13 +12,13 @@ if [[ -z ${server_jar} ]]; then
   exit 0
 else
   echo "A tentar iniciar um server de minecraft"
-  if [ ! -f /bin/ServerJars/${server_jar}.jar ]; then
+  if [ ! -f /usr/bin/ServerJars/${server_jar}.jar ]; then
     echo "O server selecionado não foi encontrado"
     echo "Por favor tente denovo"
     exit 0
   else
     echo "Server encontrado, a iniciar..."
-    cp -p /bin/ServerJars/${server_jar}.jar /home/container/server.jar
+    cp -p /usr/bin/ServerJars/${server_jar}.jar /home/container/server.jar
     java -Xmx{{SERVER_MEMORY}}M -Xms256M -jar server.jar
   fi
 fi
