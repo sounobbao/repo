8d6507f044243c3bc7680be3beee49d00e3590af
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-05-04 22:26:03 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -1,5 +1,140 @@
 #!/bin/bash
 
+function paper(){
+	echo "Selecione a sua versão do forge"
+	options=(
+	        "1.16.5"
+			"1.16.4"
+			"1.16.3"
+			"1.16.2"
+			"1.16.1"
+	        "1.15.2"
+	        "1.15.1"
+	        "1.15"
+	        "1.14.4"
+	        "1.14.3"
+	        "1.14.2"
+	        "1.14.1"
+	        "1.14"
+	        "1.13.2"
+	        "1.12.2"
+	        "1.12"
+	        "1.11.2"
+	        "1.10.2"
+	        "1.9.4"
+	        "1.8.8"
+	        "Sair"
+	)
+	select option in "${options[@]}"; do
+	    case $option in
+	        ${options[0]})
+                echo "A transferir... (PAPER: 1.16.5)"
+	            curl http://194.233.68.216:5051/download/paper-1.16.5.jar -o server.jar
+	            exit 0
+	        ;;
+			${options[1]})
+                echo "A transferir... (PAPER: 1.16.4)"
+	            curl http://194.233.68.216:5051/download/paper-1.16.5.jar -o server.jar
+	            exit 0
+	        ;;
+			${options[2]})
+                echo "A transferir... (PAPER: 1.16.3)"
+	            curl http://194.233.68.216:5051/download/paper-1.16.3.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[3]})
+                echo "A transferir... (PAPER: 1.16.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.16.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[4]})
+                echo "A transferir... (PAPER: 1.16.1)"
+	            curl http://194.233.68.216:5051/download/paper-1.16.1.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[5]})
+                echo "A transferir... (PAPER: 1.15.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.15.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[6]})
+                echo "A transferir... (PAPER: 1.15.1)"
+	            curl http://194.233.68.216:5051/download/paper-1.15.1.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[7]})
+                echo "A transferir... (PAPER: 1.15)"
+	            curl http://194.233.68.216:5051/download/paper-1.15.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[8]})
+                echo "A transferir... (PAPER: 1.14.4)"
+	            curl http://194.233.68.216:5051/download/paper-1.14.4.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[9]})
+                echo "A transferir... (PAPER: 1.14.3)"
+	            curl http://194.233.68.216:5051/download/paper-1.14.3.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[10]})
+                echo "A transferir... (PAPER: 1.14.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.14.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[11]})
+                echo "A transferir... (PAPER: 1.14.1)"
+	            curl http://194.233.68.216:5051/download/paper-1.14.1.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[12]})
+                echo "A transferir... (PAPER: 1.14)"
+	            curl http://194.233.68.216:5051/download/paper-1.14.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[13]})
+                echo "A transferir... (PAPER: 1.13.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.13.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[14]})
+                echo "A transferir... (PAPER: 1.12.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.12.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[15]})
+                echo "A transferir... (PAPER: 1.12)"
+	            curl http://194.233.68.216:5051/download/paper-1.12.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[16]})
+                echo "A transferir... (PAPER: 1.11.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.11.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[17]})
+                echo "A transferir... (PAPER: 1.10.2)"
+	            curl http://194.233.68.216:5051/download/paper-1.10.2.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[18]})
+                echo "A transferir... (PAPER: 1.9.4)"
+	            curl http://194.233.68.216:5051/download/paper-1.9.4.jar -o server.jar
+	            exit 0
+	        ;;
+	        ${options[19]})
+                echo "A transferir... (PAPER: 1.8.8)"
+	            curl http://194.233.68.216:5051/download/paper-1.8.8.jar -o server.jar
+	            exit 0
+	        ;;
+			${options[20]})
+	            echo "A sair"
+				exit 0
+	        ;;
+		esac
+	done
+}
+
 function forge(){
 	echo "Selecione a sua versão do forge"
 	options=(
