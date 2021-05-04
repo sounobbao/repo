6781fa05ad7d8b922a06dd2616c3ae93cfbcdf36
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-05-04 20:47:18 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -13,21 +13,21 @@ function forge(){
 	        ${options[0]})
                 echo "A transferir... (FORGE: 1.16.5/BUILD: RECOMENDED)"
 	            curl http://194.233.68.216:5051/download/forge-1.16.5-installer.jar -o installer.jar
-	            java -Xmx{{SERVER_MEMORY}}M -Xms128M -jar installer.jar --installServer
+	            java -Xmx${SERVER_MEMORY}M -Xms128M -jar installer.jar --installServer
 	            rm -rf installer.jar
 	            exit 0
 	        ;;
 			${options[1]})
                 echo "A transferir... (FORGE: 1.12.2/BUILD: RECOMENDED)"
 	            curl http://194.233.68.216:5051/download/forge-1.12.2-installer.jar -o installer.jar
-	            java -Xmx{{SERVER_MEMORY}}M -Xms128M -jar installer.jar --installServer
+	            java -Xmx${SERVER_MEMORY}M -Xms128M -jar installer.jar --installServer
 	            rm -rf installer.jar
 	            exit 0
 	        ;;
 			${options[2]})
                 echo "A transferir... (FORGE: 1.7.10/BUILD: RECOMENDED)"
 	            curl http://194.233.68.216:5051/download/forge-1.7.10-installer.jar -o installer.jar
-	            java -Xmx{{SERVER_MEMORY}}M -Xms128M -jar installer.jar --installServer
+	            java -Xmx${SERVER_MEMORY}M -Xms128M -jar installer.jar --installServer
 	            rm -rf installer.jar
 	            exit 0
 	        ;;
