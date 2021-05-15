67fa40d94a48ee168c91105b88fa7f20b68e4505
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-05-15 20:05:22 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -2,7 +2,7 @@
 
 function vanilla(){
 	echo "Selecione a sua versão do minecraft vanilla"
-	options=('1.16.5' '1.16.4' '1.16.3' '1.16.2' '1.16.1' '1.16' '1.15.2' '1.15.1' '1.15' '1.14.4' '1.14.3' '1.14.2' '1.14.1' '1.14' '1.13.2' '1.13.1' '1.13' '1.12.2' '1.12.1' '1.12' '1.11.2' '1.11.1' '1.11' '1.10.2' '1.10.1' '1.10' '1.9.4' '1.9.3' '1.9.2' '1.9.1' '1.9' '1.8.8' '1.8.7' '1.8.6' '1.8.5' '1.8.4' '1.8.3' '1.8.2' '1.8.1' '1.8' '1.7.10' '1.7.9' '1.7.8' '1.7.7' '1.7.6' '1.7.5' '1.7.4' '1.7.3' '1.7.2' '1.7.1' '1.7' '1.6.4' '1.6.3' '1.6.2' '1.6.1' '1.6' '1.5.2' '1.5.1' '1.5' '1.4.7' '1.4.6' '1.4.5' '1.4.4' '1.4.3' '1.4.2' '1.4.1' '1.4' '1.3.2' '1.3.1' '1.3' '1.2.5' '1.2.4' '1.2.3' '1.2.2' '1.2.1')
+	options=('1.16.5' '1.16.4' '1.16.3' '1.16.2' '1.16.1' '1.16' '1.15.2' '1.15.1' '1.15' '1.14.4' '1.14.3' '1.14.2' '1.14.1' '1.14' '1.13.2' '1.13.1' '1.13' )
     select version in "${options[@]}";
     do
     	if [[ ! " ${options[@]} " =~ " ${version} " ]]; then
@@ -33,12 +33,6 @@ function paper(){
 	        "1.14.1"
 	        "1.14"
 	        "1.13.2"
-	        "1.12.2"
-	        "1.12"
-	        "1.11.2"
-	        "1.10.2"
-	        "1.9.4"
-	        "1.8.8"
 	        "Sair"
 	)
 	select option in "${options[@]}"; do
@@ -113,37 +107,7 @@ function paper(){
 	            curl http://194.233.68.216:5051/download/paper-1.13.2.jar -o server.jar
 	            exit 0
 	        ;;
-	        ${options[14]})
-                echo "A transferir... (PAPER: 1.12.2)"
-	            curl http://194.233.68.216:5051/download/paper-1.12.2.jar -o server.jar
-	            exit 0
-	        ;;
-	        ${options[15]})
-                echo "A transferir... (PAPER: 1.12)"
-	            curl http://194.233.68.216:5051/download/paper-1.12.jar -o server.jar
-	            exit 0
-	        ;;
-	        ${options[16]})
-                echo "A transferir... (PAPER: 1.11.2)"
-	            curl http://194.233.68.216:5051/download/paper-1.11.2.jar -o server.jar
-	            exit 0
-	        ;;
-	        ${options[17]})
-                echo "A transferir... (PAPER: 1.10.2)"
-	            curl http://194.233.68.216:5051/download/paper-1.10.2.jar -o server.jar
-	            exit 0
-	        ;;
-	        ${options[18]})
-                echo "A transferir... (PAPER: 1.9.4)"
-	            curl http://194.233.68.216:5051/download/paper-1.9.4.jar -o server.jar
-	            exit 0
-	        ;;
-	        ${options[19]})
-                echo "A transferir... (PAPER: 1.8.8)"
-	            curl http://194.233.68.216:5051/download/paper-1.8.8.jar -o server.jar
-	            exit 0
-	        ;;
-			${options[20]})
+			${options[14]})
 	            echo "A sair"
 				exit 0
 	        ;;
@@ -158,8 +122,6 @@ function forge(){
 	echo "Selecione a sua versão do forge"
 	options=(
 	        "1.16.5"
-	        "1.12.2"
-	        "1.7.10"
 	        "Sair"
 	)
 	select option in "${options[@]}"; do
@@ -173,22 +135,6 @@ function forge(){
 	            exit 0
 	        ;;
 			${options[1]})
-                echo "A transferir... (FORGE: 1.12.2/BUILD: RECOMENDED)"
-	            curl http://194.233.68.216:5051/download/forge-1.12.2-installer.jar -o installer.jar
-	            java -Xmx${SERVER_MEMORY}M -Xms128M -jar installer.jar --installServer
-	            rm -rf installer.jar
-		    mv forge-1.12.2-14.23.5.2855.jar server.jar
-	            exit 0
-	        ;;
-			${options[2]})
-                echo "A transferir... (FORGE: 1.7.10/BUILD: RECOMENDED)"
-	            curl http://194.233.68.216:5051/download/forge-1.7.10-installer.jar -o installer.jar
-	            java -Xmx${SERVER_MEMORY}M -Xms128M -jar installer.jar --installServer
-	            rm -rf installer.jar
-		    mv forge-1.7.10-10.13.4.1614-1.7.10-universal.jar server.jar
-	            exit 0
-	        ;;
-			${options[3]})
 	            echo "A sair"
 				exit 0
 	        ;;
@@ -202,6 +148,8 @@ function forge(){
 main_menu () {
 	if [ ! -f ./server.jar ]; then	
 	    echo "server.jar não encontrado... selecione um server"
+		echo "ou fassa upload de um server.jar para o file manager"
+		echo "NOTA: só irao apareçer versões superiores a 1.13"
 	    options=(
 	        "Forge"
 	        "Paper"
