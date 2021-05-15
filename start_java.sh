4cf73fcb67cbeb4d80c7137e2215af1c6b556314
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-05-15 22:06:15 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function proxy(){
+	echo "Selecione o seu proxy"
+	echo "Se quiseres alguma informção sobre os tipos server vá no canal #minecraft-proxys"
+	options=('Bungeecord' 'Waterfall' 'Travertine' 'Velocity', 'Geyser')
+    select version in "${options[@]}";
+    do
+    	if [[ ! " ${options[@]} " =~ " ${version} " ]]; then
+    		echo "Essa versão não está na lista, a sair..."
+    		exit 0
+    	else
+    		echo "A transferir... (PROXY: ${version})"
+    		curl http://194.233.68.216:5051/download/proxy-${version}.jar -o server.jar
+    		exit 0
+    	fi
+    done
+}
+
 function vanilla(){
 	echo "Selecione a sua versão do minecraft vanilla"
 	options=('1.16.5' '1.16.4' '1.16.3' '1.16.2' '1.16.1' '1.16' '1.15.2' '1.15.1' '1.15' '1.14.4' '1.14.3' '1.14.2' '1.14.1' '1.14' '1.13.2' '1.13.1' '1.13' '1.12.2' '1.12.1' '1.12' '1.11.2' '1.11.1' '1.11' '1.10.2' '1.10.1' '1.10' '1.9.4' '1.9.3' '1.9.2' '1.9.1' '1.9' '1.8.8' '1.8.7' '1.8.6' '1.8.5' '1.8.4' '1.8.3' '1.8.2' '1.8.1' '1.8' '1.7.10' '1.7.9' '1.7.8' '1.7.7' '1.7.6' '1.7.5' '1.7.4' '1.7.3' '1.7.2' '1.7.1' '1.7' '1.6.4' '1.6.3' '1.6.2' '1.6.1' '1.6' '1.5.2' '1.5.1' '1.5' '1.4.7' '1.4.6' '1.4.5' '1.4.4' '1.4.3' '1.4.2' '1.4.1' '1.4' '1.3.2' '1.3.1' '1.3' '1.2.5' '1.2.4' '1.2.3' '1.2.2' '1.2.1')
@@ -199,13 +216,16 @@ function forge(){
 	done
 }
 
+
 main_menu () {
 	if [ ! -f ./server.jar ]; then	
 	    echo "server.jar não encontrado... selecione um server"
+		echo "ou fassa upload de um server.jar para o file manager"
 	    options=(
 	        "Forge"
 	        "Paper"
 	        "Vanilla"
+		"Proxys"
 	        "Sair"
 	    )
 	    select option in "${options[@]}"; do
@@ -223,7 +243,11 @@ main_menu () {
 	                break
 	             ;;
 	            ${options[3]})
-	                exit
+	                proxy
+			break
+	             ;;
+		    ${options[4]})
+	                exit 0
 	             ;;
 	            *) 
 	                echo "Opeção inválida"
