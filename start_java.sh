b3722b677d6f929bdb21de630457d4a925c5a1c3
sounobbao <48261004+sounobbao@users.noreply.github.com>
2021-05-16 23:09:01 +0100

    Update start_java.sh

--- start_java.sh
+++ start_java.sh
@@ -266,7 +266,7 @@ main_menu () {
 		printf "===================================\n"
 		java -version
 		printf "===================================\n"
-		eval java -Xmx${SERVER_MEMORY}M -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar
+		exec java -Xmx${SERVER_MEMORY}M -Xms128M -jar server.jar
 	fi
 }
 
